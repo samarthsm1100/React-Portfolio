e2fe77d5ec35d20d6c09bfe4160315556419f61c
Samarth <samarthmali1100@gmail.com>
2023-12-15 14:04:50 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+
+import Navbar from "./scenes/Navbar.jsx"
+import DotGroup from "./scenes/DotGroup.jsx";
+import Landing from "./scenes/Landing.jsx";
+import LineGradient from "./components/LineGradient.jsx"
+import MySkills from "./scenes/MySkills.jsx";
+import Projects from "./scenes/Projects.jsx"
+import Testimonials from "./scenes/Testimonials.jsx"
+import Contact from "./scenes/Contact.jsx";
+import Footer from "./scenes/Footer.jsx"
 
 function App() {
+
+  const [selectedPage, setSelectedPage] = useState();
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    setSelectedPage('home');
+  },[])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY === 0) setIsTopOfPage(true);
+      if(window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app bg-deep-blue">
+      <Navbar 
+        isTopOfPage={isTopOfPage} 
+        selectedPage = {selectedPage} 
+        setSelectedPage = {setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup 
+            selectedPage = {selectedPage}
+            setSelectedPage = {setSelectedPage}
+          />
+        )}
+        <Landing 
+          setSelectedPage = {setSelectedPage}
+        />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <MySkills />
+      </div>
+      <div className="w-5/6 mx-auto">
+        <Projects />
+      </div>
+      <div className="w-5/6 mx-auto md:h-full">
+        <Testimonials />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">  
+        <Contact />
+      </div>
+      <Footer />
     </div>
   );
 }
